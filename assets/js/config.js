d295ffcd8c222ccc0ab6c66bd6c7230e70aecd41
antomirenda <anto.mirenda1@gmail.com>
2026-08-29 01:45:39 +0200

    Configure push endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,6 +2,8 @@ const CONFIG = {
   bagId: "AM-7K42",
   siteUrl: "https://antomirenda.github.io/bag-id/bag/?id=AM-7K42",
   formEndpoint: "https://bag-id-recovery.pro-loco-san-2036.chatgpt.site/api/reports",
+  pushEndpoint: "https://bag-id-recovery.pro-loco-san-2036.chatgpt.site/api/push",
+  pushPublicKey: "BMCXu0hLfaWAK6D2oH0zDec8qOdYa7hwLzsxy5KwqVuT5HamKRX7-z8Jj61djTVfQy9y-OV4ztehO-f5mLazYY0",
   defaultLanguage: "it"
 };
 
