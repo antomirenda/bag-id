9b9262fab2a17979e183db9fab40be58c83ea853
antomirenda <anto.mirenda1@gmail.com>
2026-08-28 19:25:54 +0200

    Add configuration settings in config.js

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  bagId: "AM-7K42",
+  siteUrl: "https://antomirenda.github.io/bag-id/bag/?id=AM-7K42",
+  formEndpoint: "INSERIRE_ENDPOINT_FORMSPREE",
+  defaultLanguage: "it"
+};
+
+window.CONFIG = CONFIG;
