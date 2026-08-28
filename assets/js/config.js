23d6be9a4b588a7927e564338493800425d05f1e
antomirenda <anto.mirenda1@gmail.com>
2026-08-28 23:21:24 +0200

    Connect report form to private backend

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   bagId: "AM-7K42",
   siteUrl: "https://antomirenda.github.io/bag-id/bag/?id=AM-7K42",
-  formEndpoint: "INSERIRE_ENDPOINT_FORMSPREE",
+  formEndpoint: "https://bag-id-recovery.pro-loco-san-2036.chatgpt.site/api/reports",
   defaultLanguage: "it"
 };
 
